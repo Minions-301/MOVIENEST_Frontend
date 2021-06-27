06627147ad6fea70651ee8d0d4d2d3f0c6eb255e
AbdalrhmanBanyissa <aboood.banyissa.94@gmail.com>
2021-06-27 23:29:43 +0300

    modify the cards

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -1,93 +1,83 @@
-import React from 'react';
-import axios from 'axios';
-import MovieCard from './MovieCard';
-import { Row, Col } from 'react-bootstrap'
-import Form from 'react-bootstrap/Form';
+import './Movie.css';
+import React from "react";
+import axios from "axios";
+import MovieCard from "./MovieCard";
+import { Row, Col } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
 
 class Movie extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
-
       searchResult: [],
       mostWatched: [],
-      value: '',
+      value: "",
       direct: false,
-      category: '',
-      query: '',
-
-    }
+      category: "",
+      query: "",
+    };
   }
 
   componentDidMount() {
-
-    const url = ` https://api.themoviedb.org/3/movie/top_rated?api_key=98b1f578c2970f8efbe6ac02bd6a0cd4&language=en-US&page=1`
+    const url = ` https://api.themoviedb.org/3/movie/top_rated?api_key=98b1f578c2970f8efbe6ac02bd6a0cd4&language=en-US&page=1`;
     console.log(url);
-    axios.get(url).then(info => {
-      this.setState({
-        searchResult: info.data.results,
+    axios
+      .get(url)
+      .then((info) => {
+        this.setState({
+          searchResult: info.data.results,
+        });
       })
-    }).catch(err => {
-      console.log(err);
-    })
+      .catch((err) => {
+        console.log(err);
+      });
     this.getMostWatched();
   }
   getMostWatched = async () => {
     try {
-      const mostWatched = await axios.get(`${process.env.REACT_APP_SERVER}/mostWatched`);
+      const mostWatched = await axios.get(
+        `${process.env.REACT_APP_SERVER}/mostWatched`
+      );
       this.setState({
-        mostWatched: mostWatched
-      })
-    }
-    catch (error) {
+        mostWatched: mostWatched,
+      });
+    } catch (error) {
       console.log(error);
     }
-
-  }
-
-
+  };
 
   getApicategory = async (event) => {
-    event.preventDefault()
+    event.preventDefault();
     const url = `https://api.themoviedb.org/3/discover/movie?api_key=98b1f578c2970f8efbe6ac02bd6a0cd4&with_genres=${event.target.value}`;
     try {
       const results = await axios.get(url);
 
-
       this.setState({
         searchResult: results.data.results,
-      })
+      });
     } catch (error) {
       console.log(error);
     }
-
-
-
-  }
+  };
 
   getApiQuery = async (event) => {
-    event.preventDefault()
+    event.preventDefault();
     const url = `https://api.themoviedb.org/3/search/movie?query=${event.target.query.value}&api_key=98b1f578c2970f8efbe6ac02bd6a0cd4`;
     try {
       const results = await axios.get(url);
 
-
       this.setState({
         searchResult: results.data.results,
-      })
+      });
     } catch (error) {
       console.log(error);
     }
-
-
-  }
-
-
-
+  };
   render() {
     return (
       <>
+        <h1>Most 5 Movies Watched</h1>
         <Row className="justify-content-md-center">
           {this.state.mostWatched.map((item, idx) => (
             <Col md="auto">
@@ -96,9 +86,14 @@ class Movie extends React.Component {
           ))}
         </Row>
         <Form onSubmit={this.getApiQuery}>
-          <input className='input' name='query' placeholder='Search a film...' />
-          <button type="submit" icon="search">Search</button>
-
+          <input
+            className="input"
+            name="query"
+            placeholder="Search a film..."
+          />
+          <button type="submit" icon="search">
+            Search
+          </button>
         </Form>
         <select onChange={this.getApicategory}>
           <option value="35">comedy</option>
@@ -115,9 +110,8 @@ class Movie extends React.Component {
             </Col>
           ))}
         </Row>
-
       </>
-    )
+    );
   }
 }
 export default Movie;
