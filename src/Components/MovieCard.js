5531df7dbb4e7cac4a16d511b2ef1faec518d388
Ali Rahhal <arahal81@gmail.com>
2021-06-29 15:36:22 +0300

    Handel Add-delete-move as watched

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -9,38 +9,68 @@ class MovieCard extends React.Component {
     super(props);
     this.state = {
       addTowatchListState: [],
+      movieList: [],
     }
   }
   //continue form here
   addTowatchList = async (movie) => {
-    console.log(movie);
-    console.log(this.props.auth0.user.email);
+    console.log(movie.id);
+    const isExist = this.state.movieList.find(item => item.movie_ID == movie.id);
+    console.log(typeof isExist);
+
     if (this.props.auth0.isAuthenticated) {
-      const movieparam = {
-        movie_ID: movie.id,
-        title: movie.title,
-        overview: movie.overview,
-        moviePoster: movie.poster_path,
-        email: this.props.auth0.user.email,
-        release_date: movie.release_date,
-        vote_average: movie.vote_average,
-      }; try {
-        console.log(movieparam);
-        const addTowatchListReq = await axios.post(`${process.env.REACT_APP_SERVER}/movies`, movieparam);
-        this.setState({
-          addTowatchListState: addTowatchListReq.data,
-        })
-      } catch {
-        console.log("Error in  addTowatchList Request");
+      //console.log(typeof isExist == 'undefined');
+      if (typeof isExist == 'undefined') {
+        
+
+        const movieparam = {
+          movie_ID: movie.id,
+          title: movie.title,
+          overview: movie.overview,
+          moviePoster: movie.poster_path,
+          email: this.props.auth0.user.email,
+          release_date: movie.release_date,
+          vote_average: movie.vote_average,
+        }; try {
+
+          const addTowatchListReq = await axios.post(`${process.env.REACT_APP_SERVER}/movies`, movieparam);
+          this.setState({
+            addTowatchListState: addTowatchListReq.data,
+          })
+        } catch {
+          console.log("Error in  addTowatchList Request");
+        }
       }
+      else { console.log('is alredy Added'); }
     } else {
       console.log('please sign in');
     }
 
+    this.getMovieList();
+  }
+  componentDidMount() { if (this.props.auth0.isAuthenticated) {this.getMovieList()} }
+
+  getMovieList = async () => {
+    try {
+      const movieList = await axios.get(`${process.env.REACT_APP_SERVER}/list?email=${this.props.auth0.user.email}`)
+      this.setState({
+        movieList: movieList.data,
+      })
+    } catch {
+      console.log("Error in reviews Request");
+    }
 
   }
+
   addMovieAsWatched = async (movie) => {
+    console.log(movie.id);
+    const isExist = this.state.movieList.find(item => item.movie_ID == movie.id);
+    console.log(typeof isExist);
+
     if (this.props.auth0.isAuthenticated) {
+      //console.log(typeof isExist == 'undefined');
+      if (typeof isExist == 'undefined') {
+   
       const movieparam = {
         movie_ID: movie.id,
         title: movie.title,
@@ -50,7 +80,7 @@ class MovieCard extends React.Component {
         release_date: movie.release_date,
         vote_average: movie.vote_average,
       }; try {
-        console.log('try to send addTowatchListReq');
+        
         const addTowatchListReq = await axios.post(`${process.env.REACT_APP_SERVER}/moviesWatched`, movieparam);
         this.setState({
           addTowatchListState: addTowatchListReq.data,
@@ -58,10 +88,12 @@ class MovieCard extends React.Component {
       } catch {
         console.log("Error in  addTowatchList Request");
       }
-    } else {
-      console.log('please sign in');
     }
-
+    else { console.log('is alredy Added'); }
+  } else {
+    console.log('please sign in');
+  }
+  this.getMovieList();
 
   }
 
@@ -69,55 +101,55 @@ class MovieCard extends React.Component {
     return (
       <>
 
-      <div className='movieCardContainer'>
-        {this.props.movie && (
-          <Card className='mainMovieCard'>
-            <Card.Img
-              className='cardImg'
-              variant="top"
-              src={`https://www.themoviedb.org/t/p/w600_and_h900_bestv2${this.props.movie.poster_path}`}
-            />
-            
-            <ListGroup className="list-group-flush">
-              <ListGroupItem className ="date"><Card.Body>
-              <Card.Title> {this.props.movie.title}</Card.Title>
-            </Card.Body>
-            <br/><br/> <p> {this.props.movie.release_date}</p>
-            <br/> <p className = "vot">votes avg :{this.props.movie.vote_average}/10</p></ListGroupItem>
-              {/* <ListGroupItem className = "vote">
+        <div className='movieCardContainer'>
+          {this.props.movie && (
+            <Card className='mainMovieCard'>
+              <Card.Img
+                className='cardImg'
+                variant="top"
+                src={`https://www.themoviedb.org/t/p/w600_and_h900_bestv2${this.props.movie.poster_path}`}
+              />
+
+              <ListGroup className="list-group-flush">
+                <ListGroupItem className="date"><Card.Body>
+                  <Card.Title> {this.props.movie.title}</Card.Title>
+                </Card.Body>
+                  <br /><br /> <p> {this.props.movie.release_date}</p>
+                  <br /> <p className="vot">votes avg :{this.props.movie.vote_average}/10</p></ListGroupItem>
+                {/* <ListGroupItem className = "vote">
                 vote avg:{this.props.movie.vote_average}
               </ListGroupItem> */}
-            </ListGroup>
-            <Card.Body >
-              <div className = "buttonCard">
-              <Button
-                class="btn"
-                onClick={() => {
-                  this.addTowatchList(this.props.movie);
-                }}
-              >
-                {" "}
-                
-                ADD TO WATCH LIST
-              </Button>
-              <Button
-                class="btn"
-                onClick={() => {
-                  this.addTowatchList(this.props.movie);
-                }}
-              >
-                {" "}
-                AS Watched
-              </Button>
-              </div>
-            </Card.Body>
-          </Card>
-//           <Image src={require(`https://www.themoviedb.org/t/p/w600_and_h900_bestv2${this.props.movie.poster_path}`)} style={{width: '100%', height: '100%'}}>
-//    <View style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center'}}>
-//      <Text>Centered text</Text>
-//    </View>
-// </Image>
-        )}
+              </ListGroup>
+              <Card.Body >
+                <div className="buttonCard">
+                  <Button
+                    class="btn"
+                    onClick={() => {
+                      this.addTowatchList(this.props.movie);
+                    }}
+                  >
+                    {" "}
+
+                    ADD TO WATCH LIST
+                  </Button>
+                  <Button
+                    class="btn"
+                    onClick={() => {
+                      this.addMovieAsWatched(this.props.movie);
+                    }}
+                  >
+                    {" "}
+                    AS Watched
+                  </Button>
+                </div>
+              </Card.Body>
+            </Card>
+            //           <Image src={require(`https://www.themoviedb.org/t/p/w600_and_h900_bestv2${this.props.movie.poster_path}`)} style={{width: '100%', height: '100%'}}>
+            //    <View style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center'}}>
+            //      <Text>Centered text</Text>
+            //    </View>
+            // </Image>
+          )}
         </div>
 
       </>
